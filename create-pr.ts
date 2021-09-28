6771c7e1f66eb72ea4921143a2058ab197b992e4
kahgeh <kahgeh@hotmail.com>
2021-09-28 18:11:57 +1000

    get and save content

--- create-pr.ts
+++ create-pr.ts
@@ -1,11 +1,20 @@
 import {
-    decode as base64Decode,
-} from 'https://deno.land/std/encoding/base64.ts';
+  parse as yamlParse,
+  parseAll as yamlParseAll,
+  stringify as yamlStringify,
+} from "https://deno.land/std/encoding/yaml.ts";
+import {
+  decode as base64Decode,
+  encode as base64Encode,
+} from "https://deno.land/std/encoding/base64.ts";
 import { token } from "./creds.ts";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core?dts";
-const baseUrl = 'https://api.github.com';
-const mainBranchName = 'main';
-const type = 'users'; // 'user'|'org'
+import { TextContent } from "./types/bot.ts";
+
+const baseUrl = "https://api.github.com";
+const mainBranchName = "main";
+const type = "users"; // 'user'|'org'
+
 const ghClient = new Octokit({
   baseUrl,
   auth: token,
@@ -13,42 +22,102 @@ const ghClient = new Octokit({
 const root = "kahgeh";
 const repo = "study-spork";
 
-async function branchOffMaster(repo: string, owner: string, sourceBranchName: string, newBranchRef: string) {
-  const main = await ghClient.request(`GET /repos/{owner}/{repo}/git/ref/{ref}`, {
-    owner,
-    repo,
-    ref: `heads/${sourceBranchName}`
-  })
+async function branchOffMaster(
+  repo: string,
+  owner: string,
+  sourceBranchName: string,
+  newBranchRef: string,
+) {
+  const main = await ghClient.request(
+    `GET /repos/{owner}/{repo}/git/ref/{ref}`,
+    {
+      owner,
+      repo,
+      ref: `heads/${sourceBranchName}`,
+    },
+  );
 
   return await ghClient.request(`POST /repos/{owner}/{repo}/git/refs`, {
     owner,
-    repo, 
+    repo,
     ref: newBranchRef,
-    sha: main.data.object.sha 
-    });
-
+    sha: main.data.object.sha,
+  });
 }
 
-async function getContent(repo: string, owner: string, path: string){
-  const response = await ghClient.request('GET /repos/{owner}/{repo}/contents/{path}', {
+async function getContent(
+  repo: string,
+  owner: string,
+  path: string,
+): Promise<TextContent> {
+  const response = await ghClient.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner,
+      repo,
+      path,
+    },
+  );
+  const responseData = (response.data as any);
+  const { content, sha } = responseData;
+  const textDecoder = new TextDecoder("utf-8");
+  return { text: textDecoder.decode(base64Decode(content)), sha };
+}
+
+function saveContent(
+  content: TextContent,
+  path: string,
+  branch: string,
+  message: string,
+  repo: string,
+  owner: string,
+) {
+  const { text, sha } = content;
+
+  const base64Content = base64Encode(text);
+  return ghClient.request("PUT /repos/{owner}/{repo}/contents/{path}", {
     owner,
     repo,
-    path
+    path,
+    message,
+    content: base64Content,
+    sha,
+    branch,
   });
-  return base64Decode((response.data as any).content);
+}
+
+function modifyYamlContent(originalContent: string) {
+  const data = yamlParse(originalContent) as any;
+  data.field2 = "something fancy";
+  return yamlStringify(data);
 }
 
 async function main(): Promise<void> {
   // const repositories = await ghClient.request(`GET /${type}/{org}/repos`, { org });
   // console.log(JSON.stringify(repositories));
 
-  const newBranchName = 'test';
-  const newBranch = await branchOffMaster(repo, root, mainBranchName, `refs/heads/${newBranchName}`);
+  const newBranchName = "test";
+  const newBranch = await branchOffMaster(
+    repo,
+    root,
+    mainBranchName,
+    `refs/heads/${newBranchName}`,
+  );
   console.log(JSON.stringify(newBranch));
-  const path = 'some.yml';
-  const content = await getContent(repo, root, path);
-  const textDecoder = new TextDecoder('utf-8');
-  console.log(textDecoder.decode(content));
+  const path = "some.yml";
+  const message = "update to something fancy";
+  const originalContent = await getContent(repo, root, path);
+  const modifiedContent = modifyYamlContent(originalContent.text);
+  console.log(`modified ${originalContent} to ${modifiedContent}`);
+  const saveResult = await saveContent(
+    { text: modifiedContent, sha: originalContent.sha },
+    path,
+    newBranchName,
+    message,
+    repo,
+    root,
+  );
+  console.log(JSON.stringify(saveResult));
   return;
 }
 
---
+++ types/bot.ts
@@ -0,0 +1,4 @@
+interface TextContent {
+  text: string,
+  sha: string,
+}
