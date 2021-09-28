787ae4aed08cdd65b1ca02e0571fb18566e51fc4
kahgeh <kahgeh@hotmail.com>
2021-09-28 16:41:37 +1000

    get content to update

--- create-pr.ts
+++ create-pr.ts
@@ -1,3 +1,6 @@
+import {
+    decode as base64Decode,
+} from 'https://deno.land/std/encoding/base64.ts';
 import { token } from "./creds.ts";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core?dts";
 const baseUrl = 'https://api.github.com';
@@ -10,7 +13,7 @@ const ghClient = new Octokit({
 const root = "kahgeh";
 const repo = "study-spork";
 
-async function branchOffMaster(repo: string, owner: string, sourceBranchName: string, newBranchName: string) {
+async function branchOffMaster(repo: string, owner: string, sourceBranchName: string, newBranchRef: string) {
   const main = await ghClient.request(`GET /repos/{owner}/{repo}/git/ref/{ref}`, {
     owner,
     repo,
@@ -20,17 +23,32 @@ async function branchOffMaster(repo: string, owner: string, sourceBranchName: st
   return await ghClient.request(`POST /repos/{owner}/{repo}/git/refs`, {
     owner,
     repo, 
-    ref: newBranchName,
+    ref: newBranchRef,
     sha: main.data.object.sha 
     });
 
 }
 
+async function getContent(repo: string, owner: string, path: string){
+  const response = await ghClient.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    owner,
+    repo,
+    path
+  });
+  return base64Decode((response.data as any).content);
+}
+
 async function main(): Promise<void> {
   // const repositories = await ghClient.request(`GET /${type}/{org}/repos`, { org });
   // console.log(JSON.stringify(repositories));
-  const newBranch = await branchOffMaster(repo, root, mainBranchName, 'refs/heads/test');
+
+  const newBranchName = 'test';
+  const newBranch = await branchOffMaster(repo, root, mainBranchName, `refs/heads/${newBranchName}`);
   console.log(JSON.stringify(newBranch));
+  const path = 'some.yml';
+  const content = await getContent(repo, root, path);
+  const textDecoder = new TextDecoder('utf-8');
+  console.log(textDecoder.decode(content));
   return;
 }
 
